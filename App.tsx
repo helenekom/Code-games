1e73d8dee0974228c1d8f580da48306ed85d190d
helenekom <helenekom3@gmail.com>
2024-01-21 12:26:57 +0100

    debut

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import {Welcome} from '././src/screens/welcome'
+import { View } from 'react-native';
+
+export class APP extends Component{
+
+  render(){
+    return (
+    <View > 
+       <Welcome />  
+    </View>)
+}
+  
+}
